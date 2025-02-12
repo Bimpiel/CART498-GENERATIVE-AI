25dec5e9b10c75d60a42f669bc17f22f827b7ec7
Bimpiel <saidooazhar@gmail.com>
2025-02-12 11:51:16 -0500

    Update

--- A04/Jungian dream analysis machine/app.py
+++ A04/Jungian dream analysis machine/app.py
@@ -3,17 +3,6 @@ import openai
 import os
 from dotenv import load_dotenv
 
-import OpenAI
-client = OpenAI()
-
-response = client.images.generate(
-    model="dall-e-3",
-    prompt="a white siamese cat",
-    size="1024x1024",
-    quality="standard",
-    n=1,
-)
-
 print(response.data[0].url)
 
 load_dotenv()  # Load environment variables from .env
